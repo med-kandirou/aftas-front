31dd5689046da4cb95c69a350e2be5e93faf8f00
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2023-12-13 11:00:12 +0100

    check date to display hunt and add participant

--- src/app/modules/admin/components/competition-section/competition-section.component.ts
+++ src/app/modules/admin/components/competition-section/competition-section.component.ts
@@ -29,14 +29,7 @@ export class CompetitionSectionComponent {
     })
   }
 
-  getDayDifference(startDate: Date) :Boolean{
-    let date1 = new Date(startDate);
-    let currentDate = new Date(); 
-    let differenceInTime =date1.getTime()-currentDate.getTime();
-    let differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24));
-    return differenceInDays>=1;
-  }
-
+  
   changefiltre(filtre: any) {
     this.currentPage=0;
     this.currentSize=3;
--- src/app/modules/admin/components/list-participant/list-participant.component.ts
+++ src/app/modules/admin/components/list-participant/list-participant.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Ranking } from 'src/app/core/models/ranking.model';
 import { RankingService } from 'src/app/core/services/ranking.service';
 import { ActivatedRoute  } from '@angular/router';
+import { CompetitionService } from 'src/app/core/services/competition.service';
+import { Competition } from 'src/app/core/models/competition.model';
 
 @Component({
   selector: 'app-list-participant',
@@ -9,24 +11,42 @@ import { ActivatedRoute  } from '@angular/router';
   styleUrls: ['./list-participant.component.css']
 })
 export class ListParticipantComponent {
-  constructor(private servRanking:RankingService,private ActivatedRoute :ActivatedRoute){}
+
+  constructor(private servRanking:RankingService,private servCompetition:CompetitionService,private ActivatedRoute :ActivatedRoute){}
 
   rankings:Ranking[]
   code:string
-  possibleToadd:boolean;
-  dateCompetition:Date;
-  array:any
-  myArray:any;
+  checkday:number;
+
+
   ngOnInit(): void {
     this.code=this.ActivatedRoute.snapshot.params['codeComp']
     this.getRankings(this.code);
+    this.getCompetition(this.code);
   }
 
   getRankings(code:string){
     this.servRanking.getRankingsbyComp(code).subscribe((data:Ranking[])=>{
       this.rankings=data;
-      console.log(this.code.substring(3, this.code.length))
     });
   }
+
+  getCompetition(code:string){
+    this.servCompetition.getOne(code).subscribe((data:Competition)=>{
+      this.checkday=this.getDayDifference(data.date);
+    });
+  }
+
+
+  getDayDifference(date: Date):number{
+    let dateCompetition = new Date(date);
+    let currentDate = new Date(); 
+    dateCompetition.setHours(0, 0, 0, 0);
+    currentDate.setHours(0, 0, 0, 0);
+    let differenceInTime =dateCompetition.getTime()-currentDate.getTime();
+    let differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24));
+    return differenceInDays;
+  }
+
 
 }
